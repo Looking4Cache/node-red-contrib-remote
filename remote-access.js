5976e28a5a20a97811c8564a83ff0d7b9adc3917
Thorsten Heilmann <thorsten@looking4cache.com>
2020-10-18 18:58:31 +0000

    translations

--- remote-access.js
+++ remote-access.js
@@ -11,7 +11,7 @@ module.exports = function(RED) {
       const sshprocess = child_process.spawn("ssh", ['-o StrictHostKeyChecking=no', '-R', port.toString() + ':' + node.confignode.host + ':' + node.confignode.port.toString(), 'forward@proxy-' + server, '-N']);
 
       // TODO: Herausfinden ob wirklich verbunden
-      node.status({fill:"green",shape:"dot",text:"serving"});
+      node.status({fill:"green",shape:"dot",text:"remote-access.status.serving"});
       node.serving = true
 
       sshprocess.stdout.on('data', (data) => {
@@ -23,13 +23,13 @@ module.exports = function(RED) {
       });
 
       sshprocess.on('close', (code, signal) => {
-        node.status({fill:"red",shape:"dot",text:"stopped"});
+        node.status({fill:"red",shape:"dot",text:"remote-access.status.stopped"});
         node.serving = false
         node.log("ssh process stopped");
       });
 
       node.on('close', function() {
-        node.status({fill:"red",shape:"dot",text:"stopping"});
+        node.status({fill:"red",shape:"dot",text:"remote-access.status.stopping"});
         node.log("stopping ssh process");
         sshprocess.kill();
       });
@@ -56,7 +56,7 @@ module.exports = function(RED) {
     })
     .catch((error) => {
       node.error('axios error: ' + error);
-      node.status({fill:"red",shape:"dot",text:"Error communicating with server."});
+      node.status({fill:"red",shape:"dot",text:"remote-access.status.commerror"});
       return;
     });
   }
@@ -74,7 +74,7 @@ module.exports = function(RED) {
     node.serving = false;
 
     // Status
-    node.status({fill:"orange",shape:"dot",text:"starting"});
+    node.status({fill:"orange",shape:"dot",text:"remote-access.status.starting"});
 
     // Retrieve the config node
     node.confignode = RED.nodes.getNode(config.confignode);
@@ -84,12 +84,12 @@ module.exports = function(RED) {
       node.log("this.confignode.server: " + node.confignode.server);
     } else {
       node.log("No configuration found.");
-      node.status({fill:"red",shape:"dot",text:"No configuration found."});
+      node.status({fill:"red",shape:"dot",text:"remote-access.status.noconfig"});
       return;
     }
     if ( node.confignode.instancehash === undefined || node.confignode.instancehash === '' ) {
       node.log("Configuration incomplete.");
-      node.status({fill:"red",shape:"dot",text:"Configuration incomplete."});
+      node.status({fill:"red",shape:"dot",text:"remote-access.status.incompleteconfig"});
       return;
     }
 
@@ -103,16 +103,6 @@ module.exports = function(RED) {
     node.on('close', function() {
       clearInterval(node.servingInterval);
     });
-
-    // node.log("this.credentials.installhash: " + this.credentials.installhash);
-    // var installhash = this.credentials.installhash;
-    // if ( installhash === undefined || installhash === '' ) {
-    //  installhash = makeid(48)
-    //  this.credentials.installhash = installhash
-    //}
-    //node.log("this.credentials.installhash: " + this.credentials.installhash);
-
-    // this.status({fill:"green",shape:"dot",text:installhash});
   }
 
   RED.nodes.registerType("remote-access",RemoteAccessNode);
--- remote-config.js
+++ remote-config.js
@@ -7,7 +7,6 @@ module.exports = function(RED) {
 
   function RemoteConfigNode(n) {
     RED.nodes.createNode(this,n);
-
     this.name = n.name;
     this.host = n.host;
     this.port = n.port;
